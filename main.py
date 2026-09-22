3c035a4c63e78f04b492ae8ffc3aebc590ee705d
Idskg <akshatmyid@gmail.com>
2026-09-22 15:23:45 +0530

    started a basic template for the project

---
+++ main.py
@@ -0,0 +1,9 @@
+import whisper
+import sounddevice as sd
+
+def main():
+    if (__name__ == "__main__"):
+
+        model = whisper.load_model("medium") # Load the whisper model before doing any dumb stuff
+        # We need to check for an available nvidia gpu
+main()
