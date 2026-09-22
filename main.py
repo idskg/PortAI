74e4d5a7a2ed3a2cf9a3d7a01f4a5cce7622acc7
Idskg <akshatmyid@gmail.com>
2026-09-22 16:58:34 +0530

    added model selection
    model is selected according to the specifications

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
 import whisper
 import sounddevice as sd
+import subprocess
+import torch
+
+def gpucheck():
+    # check for available nvidia gpu
+    try:
+        subprocess.check_output(["nvidia-smi"])
+        return True
+    except FileNotFoundError:
+        return False
+
+def vramCheck():
+    try:
+        total_vram = torch.cuda.get_device_properties(0).total_memory / (1024 ** 3)
+        return total_vram
+    except Exception as e:
+        return -1
 
 def main():
     if (__name__ == "__main__"):
 
-        model = whisper.load_model("medium") # Load the whisper model before doing any dumb stuff
-        # We need to check for an available nvidia gpu
+        if gpucheck():
+            vram = vramCheck()
+            match vram:
+                case vram if vram <= 3.0:
+                    model = whisper.load_model("small", device="cuda")
+                case vram if vram <= 7.0:
+                    model = whisper.load_model("turbo", device="cuda")
+                case vram if vram <= 15.0:
+                    model = whisper.load_model("large-v3", device="cuda")
+                case _:
+                    model = whisper.load_model("base", device="cuda")
+        else:
+            model = whisper.load_model("base", device="cpu")
 main()
